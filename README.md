28c61cdb48037b8be527ca319489d65c4df47987
Ahmed Saber <ahmed0saber33@gmail.com>
2022-11-29 22:33:16 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # navigate-object-by-path-in-javascript
+inspired by **Retrieving Translation Strings in Laravel Localization**
---
+++ main.js
@@ -0,0 +1,56 @@
+const __ = (path, lang) => {
+    const translations = {
+        alerts: {
+            win: {
+                ar: "لقد فوزت",
+                en: "you win"
+            },
+            lose: {
+                ar: "لقد خسرت",
+                en: "you lose"
+            },
+            draw: {
+                ar: "لقد تعادلت",
+                en: "It's a draw"
+            }
+        },
+        auth: {
+            login: {
+                title: {
+                    ar: "قم بتسجيل الدخول للاستمرار",
+                    en: "login to continue"
+                }
+            },
+            register: {
+                title: {
+                    ar: "قم بإنشاء حساب للاستمرار",
+                    en: "create an account to continue"
+                }
+            }
+        }
+    }
+
+    const pathAsArray = path.split(".")
+    let currentTranslation = translations
+    for(let i = 0; i < pathAsArray.length; i++){
+        if(currentTranslation[pathAsArray[i]]) currentTranslation = currentTranslation[pathAsArray[i]]
+        else return {ar:"الترجمة غير متاحة", en:"Translation is not available"}[lang]
+    }
+
+    return currentTranslation[lang]
+}
+
+console.log(__('alerts.win', 'ar'))
+console.log(__('alerts.win', 'en'))
+console.log(__('alerts.lose', 'ar'))
+console.log(__('alerts.lose', 'en'))
+console.log(__('alerts.draw', 'ar'))
+console.log(__('alerts.draw', 'en'))
+console.log(__('auth.login.title', 'ar'))
+console.log(__('auth.login.title', 'en'))
+console.log(__('auth.register.title', 'ar'))
+console.log(__('auth.register.title', 'en'))
+
+// that's how to handle errors
+console.log(__('alerts.register.title', 'ar'))
+console.log(__('alerts.register.title', 'en'))
